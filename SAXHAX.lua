5767969ac04d024329db612a1b6135311848927c
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-06-28 20:13:58 +0700

    Update SAXHAX.lua

--- SAXHAX.lua
+++ SAXHAX.lua
@@ -13,9 +13,9 @@ local UI = v0:CreateWindow({
 })
 WTF1 = UI:AddTab("Main")
 OMG1 = WTF1:AddLeftGroupbox("Player")
-OMG1:AddSlider("MySlider",{Text="WalkSpeed",Min=16,Default=16,Max=21,Rouding=1,Compact=true,Callback=function(Value)Speed = Value end})
-OMG1:AddSlider("MySlider",{Text="Field Of View",Min=70,Default=70,Max=120,Rouding=1,Compact=true,Callback=function(Value)FOV = Value end})
-OMG1:AddSlider("MySlider",{Text="JumpPower",Min=50,Default=75,Max=75,Rouding=1,Compact=true,Callback=function(Value)Jump = Value end})
+OMG1:AddSlider("MySlider",{Text="WalkSpeed",Min=16,Default=16,Max=21,Rounding=1,Compact=true,Callback=function(Value)Speed = Value end})
+OMG1:AddSlider("MySlider",{Text="Field Of View",Min=70,Default=70,Max=120,Rounding=1,Compact=true,Callback=function(Value)FOV = Value end})
+OMG1:AddSlider("MySlider",{Text="JumpPower",Min=50,Default=75,Max=75,Rounding=1,Compact=true,Callback=function(Value)Jump = Value end})
 OMG1:AddToggle("MyToggle",{Text="God Mode Upper",Default=false,Callback=function(Value)
 if Value then
 local Collison = game.Players.LocalPlayer.Character:FindFirstChild("Collision")
